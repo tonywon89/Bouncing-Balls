043e647429b3bf0b3060370b56c8586dcdd4ea48
Tony Won <tonywon89@gmail.com>
2015-07-14 20:03:11 -0400

    Add various inputs for bouncing balls

--- experiment4-bouncing-ball.js
+++ experiment4-bouncing-ball.js
@@ -1,7 +1,86 @@
-var circle = $('.circle');
+//var circle = $('.circle');
 
 $(document).ready(function(){
+  var color = $('#color').val();
+  var diam = $("#diam").val();
+  var bounce = $('#bounce').val();
+  var bounceH = $('#bounceH').val();
+  var bounceS = $('#bounceS').val();
 
+/*
+function changeValue(name, property) {
+  $('#' + name).change(function(){
+    property = $('#' + name).val();
+  });
+}
+*/
+
+
+  $("#color").change(function(){
+    color = $('#color').val();
+  });
+  $("#diam").change(function(){
+    diam = $('#diam').val();
+  });
+  $("#bounce").change(function(){
+    bounce = $('#bounce').val();
+  });
+  $("#bounceH").change(function(){
+    bounceH = $('#bounceH').val();
+  });
+  $("#bounceS").change(function(){
+    bounceS = $('#bounceS').val();
+  });
+
+
+
+/*
+  $("#button2").click(function(){
+    event.stopPropagation();
+    bounce = $('#bounce').val();
+    bounceH = $('#bounceH').val();
+    bounceS = $('#bounceS').val();
+  })
+
+  //Sets the colo and size of the ball when made
+  $("#button1").click(function(){
+    event.stopPropagation();
+    color = $('#color').val();
+    diam = $("#diam").val();
+  });
+*/
+//Adds a new ball
+  $('#button3').on("click", function(){
+    $('<div class="circle"></div>').css("background-color", color).css("width", diam).css("height", diam).insertAfter("body");
+    
+  });
+    
+  
+
+  $(document).on("mouseover", ".circle", function(event){
+    event.stopPropagation();
+    for (var i=0; i < bounce; i++){
+      $(this).animate({top: "+=" + bounceH}, 100000/parseInt(bounceS));
+      $(this).animate({top: "-=" + bounceH}, 100000/parseInt(bounceS));
+    }
+  });
+  $(document).on("click", ".circle", function(event){
+    event.stopPropagation();
+    $(this).remove();
+  } );
+    
+    
+    
+
+  
+
+
+
+
+});
+
+
+/*
   var ySpeed = 20, xSpeed = 30;
   var fallSpeed = 500;
   var lost = 10;
@@ -23,6 +102,4 @@ $(document).ready(function(){
     }
     
   })
-
-
-});
+*/
